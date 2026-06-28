1221df029ebf8b08e19a04e810114a836fe0c55f
Bus Time-BLG <balangodabustime@gmail.com>
2026-06-29 01:26:14 +0530

    Add hero.png to service worker cache

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const ASSETS = [
   './manifest.json',
   './icon.png',
   './bus.svg',
-  
+  './hero.png', 
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
